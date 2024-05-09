1293c85d72f749bb353f692d5c0b9408d3059175
ozgur <ozgur.keskin@ulakhaberlesme.com.tr>
2024-05-09 21:17:08 +0300

    Fix header definitions for debug.h file.

--- src/debug.h
+++ src/debug.h
@@ -1,3 +1,6 @@
+#ifndef _WG_DEBUG_H
+#define _WG_DEBUG_H
+
 #define WG_DISABLE_LOGGING
 
 #ifdef WG_DISABLE_LOGGING
@@ -6,4 +9,6 @@
 #else
 #define WG_LOG(message, ...) do { pr_info("WG_LOG: " message " (%s:%d)", ##__VA_ARGS__, __func__, __LINE__); } while (0)
 #define WG_LOG_FUNCTION() do { pr_info("WG_LOG: %s:%d", __func__, __LINE__); } while (0)
+#endif
+
 #endif
