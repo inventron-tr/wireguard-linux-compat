3826daded37b5b666bcd12e93436487293af9171
Inventron-TR <erhan.yigitbasi@pay-port.com>
2024-05-13 12:30:19 +0300

    version str

--- src/main.c
+++ src/main.c
@@ -51,7 +51,7 @@ static int __init wg_mod_init(void)
 		goto err_netlink;
 
 	pr_info("WireGuard " WIREGUARD_VERSION " loaded. See www.wireguard.com for information.\n");
-	pr_info("Copyright (C) 2015-2019 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.\n");
+	pr_info("Copyright (C) Inventron Compiled.\n");
 
 	return 0;
 
